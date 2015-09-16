4b78e0bada944a317b09a2c6e8f0e84033e82450
Ian Candy <iancandysemail@gmail.com>
2015-09-16 15:53:47 -0400

    reorder controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,19 +12,20 @@ class ApplicationController < Sinatra::Base
 		erb :index
 	end
 
-	get "/login" do
-		erb :login
-	end
-
-	post "/login" do
-		#your code here!
-	end
-
 	get "/signup" do
 		erb :signup
 	end
 
 	post "/signup" do
+		#your code here!
+	end
+
+
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
 		#your code here!
 	end
 
