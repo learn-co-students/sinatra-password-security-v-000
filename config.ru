90c25ad7c8f68a3405f4259ddd774c1f56216b8f
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-30 14:54:20 -0500

    tests

--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
-require './app/controllers/application_controller'
+require './config/environment'
+
+if ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+end
 
 run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
-require 'bundler'
-Bundler.require
+ENV['SINATRA_ENV'] ||= "development"
 
-configure :development do
-	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
-end
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
