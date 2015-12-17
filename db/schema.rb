23249141acdadf045ce0260ebc3d43a536479d1e
onyoo <roberto9m9@gmail.com>
2015-12-17 22:45:04 +0100

    finished but some tests don't fly

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916154312) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
