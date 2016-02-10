0ea628f84f7475a0dd2dbc7627d5bdf581b3658e
pajamaw <pjpeterww@gmail.com>
2016-02-09 20:35:22 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ require "./app/models/user"
 class ApplicationController < Sinatra::Base
 
 	configure do
-		set :views, "app/views"
-		enable :sessions
+		set :views,  Proc.new { File.join(root, "../views/") }
+		enable :sessions unless test?
 		set :session_secret, "password_security"
 	end
 
@@ -17,7 +17,12 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(username: params[:username], password: params[:password])
+		if user.save
+			redirect '/login'
+		else
+			redirect '/failure'
+		end
 	end
 
 
@@ -26,7 +31,13 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect '/success'
+		else
+			redirect '/failure'
+		end
 	end
 
 	get "/success" do
