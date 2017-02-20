278da5ff30ff91bed6d9d86245ecebba4646c94e
vanita eckert <vanita112003@gmail.com>
2017-02-20 03:16:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,11 +33,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
-      redirect "/success"
-    else
-      redirect "/failure"
-    end
+			if user
+	      redirect "/success"
+	    else
+	      redirect "/failure"
+	    end
 	end
 
 	get "/success" do
