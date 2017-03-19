b0117b172d0b2d0b3a7b67cbdfbca3b9bf4d5d1f
bentonwong <bentonwong@gmail.com>
2017-03-18 19:07:46 -0700

    add functions to post /login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,12 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		#your code here!
 		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/success" do
