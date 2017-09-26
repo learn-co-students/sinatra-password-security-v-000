d5da8c0c9c42ca58f31c02e62e69e5d90f01d8c9
Tony <github email address>
2017-09-26 11:15:16 -0400

    Done.

--- db/migrate/20150916154312_create_users.rb
+++ db/migrate/20150916154312_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
 
-    def change
+    def up
         create_table :users do |t|
             t.string :username
             t.string :password_digest
         end
+    end
+ 
+    def down
+        drop_table :users
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150916154312) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
 end
