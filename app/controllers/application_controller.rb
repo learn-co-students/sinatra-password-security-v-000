48ce0f99726927ef7fc827d8ff85e4678aa1048a
Sophia H <shhubeny@gmail.com>
2017-10-17 20:02:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,36 +9,46 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/" do
-		erb :index
+		erb :index #file has links to signup or login
 	end
 
 	get "/signup" do
-		erb :signup
+		erb :signup #if user hasn't had a login, they go here, another form to signup
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login" #what happens at login? if we can find it, success
+		else
+			redirect "/failure"
+		end
 	end
 
-
 	get "/login" do
-		erb :login
+		erb :login #if person has login, they go here, enter username and password
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/success"
+    else
+        redirect "/failure"
+    end
 	end
 
 	get "/success" do
 		if logged_in?
-			erb :success
+			erb :success #displayed once user successfully logs in
 		else
-			redirect "/login"
+			redirect "/login" #mistake -login again
 		end
 	end
 
 	get "/failure" do
-		erb :failure
+		erb :failure #accessed if error logging in or signing up
 	end
 
 	get "/logout" do
@@ -47,13 +57,12 @@ class ApplicationController < Sinatra::Base
 	end
 
 	helpers do
-		def logged_in?
+		def logged_in? #helper methods
 			!!session[:user_id]
 		end
 
-		def current_user
+		def current_user #helper methods
 			User.find(session[:user_id])
 		end
 	end
-
 end
