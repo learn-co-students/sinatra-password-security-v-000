3f5b9a5b8c230328e69c87d93f463a8e74a0047b
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-11 22:04:28 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,16 +17,27 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		@user = User.new(:username => params[:username], :password => params[:password])
+		if @user.save # when does this save and when doesn't it?
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
-
+	# how is password set to invisible? what does authenticate do?
 	get "/login" do
 		erb :login
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/success" do
