7c4480773bbfdc5e9eabbb7df3d1160e2d8bea09
maxwell <github email address>
2018-03-14 16:08:34 -0400

    added require capybara to spec_helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314210852) do
+ActiveRecord::Schema.define(version: 20150916154312) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
 
 end
