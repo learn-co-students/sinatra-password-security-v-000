acc879443acbf8dd32faecfd401879888ac82828
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-17 10:44:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,58 +1,69 @@
-require "./config/environment"
-require "./app/models/user"
+require './config/environment'
+require './app/models/user'
+# Routing
 class ApplicationController < Sinatra::Base
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'password_security'
+  end
 
-	configure do
-		set :views, "app/views"
-		enable :sessions
-		set :session_secret, "password_security"
-	end
+  get '/' do
+    erb :index
+  end
 
-	get "/" do
-		erb :index
-	end
+  get '/signup' do
+    erb :signup
+  end
 
-	get "/signup" do
-		erb :signup
-	end
+  post '/signup' do
+    user = User.new(username: params[:username], password: params[:password])
 
-	post "/signup" do
-		#your code here!
-	end
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
 
-	get "/login" do
-		erb :login
-	end
+  get '/login' do
+    erb :login
+  end
 
-	post "/login" do
-		#your code here!
-	end
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/success'
+    else
+      redirect '/failure'
+    end
+  end
 
-	get "/success" do
-		if logged_in?
-			erb :success
-		else
-			redirect "/login"
-		end
-	end
+  get '/success' do
+    if logged_in?
+      erb :success
+    else
+      redirect '/login'
+    end
+  end
 
-	get "/failure" do
-		erb :failure
-	end
+  get '/failure' do
+    erb :failure
+  end
 
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
-	helpers do
-		def logged_in?
-			!!session[:user_id]
-		end
+  helpers do
+    def logged_in?
+      session.key?(:user_id)
+    end
 
-		def current_user
-			User.find(session[:user_id])
-		end
-	end
-
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
