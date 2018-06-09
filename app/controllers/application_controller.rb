264318b46054673208e10a0fb5ee5773e816615f
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-06-09 15:17:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		@user = User.find_by(username: params[:username])
-		if @user != nil && @user.authenticate(params[:password])
+		if !@user.nil? && @user.authenticate(params[:password])
 			session[:user_id] = @user.id
 			redirect '/success'
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_14_210852) do
+ActiveRecord::Schema.define(version: 2018_06_09_151021) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.decimal "balance", default: "0.0"
   end
 
 end
