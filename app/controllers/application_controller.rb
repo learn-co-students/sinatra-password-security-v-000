a458afa8cd47a756dd361fd3ecc227f5da7abd2a
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-02 02:10:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,9 @@ end
 
 post "/login" do
   user = User.find_by(:username => params[:username])
-  if user
+ 
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
     redirect "/success"
   else
     redirect "/failure"
