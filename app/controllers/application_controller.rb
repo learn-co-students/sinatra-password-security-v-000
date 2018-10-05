8c80de62b78f772722c574eaf569a43965c392e8
btmccollum <btmcco8@gmail.com>
2018-10-05 17:29:04 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,15 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+		#cant use create b/c class User has 'has_secure_password' which wont save
+		#to db unless our user filled out the pswd field
+
+		if user.save
+			redirect "/login"
+		else 
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
@@ -25,7 +33,14 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else 
+			redirect "/failure"
+		end
 	end
 
 	get "/success" do
