18c45ad5fc4a2ad976b5932b9f497a21ab8dcf09
SMartinSci <sm674@cornell.edu>
2018-11-06 03:07:49 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,13 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+ 
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
@@ -25,7 +31,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+ 		user = User.find_by(:username => params[:username])
 	end
 
 	get "/success" do
