d01d0a5c409182600932dc826c47fb6d32308127
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-02-19 19:12:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,18 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
+
+		#Note that even though our database has a column called password_digest, we still access the attribute of password. This is given to us by has_secure_password.
+		#Because our user has has_secure_password, we won't be able to save this to the database unless our user filled out the password field.
+		#Calling user.save will return false if the user can't be persisted.
+		#Let's update this route so that we redirect to '/login' if the user is saved, or '/failure' if the user can't be saved. (For now, we'll make the user log in after they sign up successfully).
+		#ext, we need to check two conditions: first, did we find a user with that username? This can be written as user != nil or simply user.
 	end
 
 	get "/login" do
@@ -26,6 +38,19 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
+		user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
+		#In the code above,
+		#we see how we can ensure that we have a User AND that that User is authenticated.
+		#If the user authenticates,
+		#we'll set the session[:user_id] and redirect to the /success route.
+		#Otherwise, we'll redirect to the /failure route so our user can try again.
 	end
 
 	get "/success" do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
+	#A macro is a method that when called, creates methods for you. This is meta programming, which you don't need to worry about now. 
+	# the macro has_secure_password is being called just like a normal ruby method.
+	#It works in conjunction with a gem called bcrypt and gives us all of those abilities in a secure way that doesn't actually store the plain text password in the database.
 end
