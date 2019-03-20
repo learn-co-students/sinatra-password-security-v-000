eac62c7808607f3d5f29f36cb52feea64df574d6
Stef Claus <stef.claus@gmail.com>
2019-03-20 23:04:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,14 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-  user = User.new(:username => params[:username], :password => params[:password])
+  	user = User.new(:username => params[:username], :password => params[:password])
 
-  if user.save
-    redirect "/login"
-  else
-    redirect "/failure"
-  end
-end
+  	if user.save
+    	redirect "/login"
+  	else
+    	redirect "/failure"
+  	end
+	end
 
 	get "/login" do
 		erb :login
