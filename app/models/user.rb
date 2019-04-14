8c95a8903865ad96772344929362cb977a813921
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-14 17:55:28 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password #ActiveRecord macro gives us access to a few new methods; macro = method that creates methods for you
+	#this macro works in conjunction w/ bcrypt gem
 	
 end
