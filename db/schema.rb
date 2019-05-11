148489bfeaca5360714b5ac61eed703ad618f98d
Heidi Hlavinka <hchlav@gmail.com>
2019-05-11 21:23:49 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314210852) do
+ActiveRecord::Schema.define(version: 20190511212214) do
 
   create_table "user", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
   end
