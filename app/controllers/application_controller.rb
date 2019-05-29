7d22ab8ec3c8aa0f62bcb77bbb7a121ccd6fe4ed
Reno Sorensen <renojames19@gmail.com>
2019-05-29 00:57:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(:username = params[:username], :password - params[:password])
+		user = User.new(:username => params[:username], :password => params[:password])
 	end
 
 	get "/login" do
