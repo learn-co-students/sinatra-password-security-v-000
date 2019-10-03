1a4545423b71ac0cfd81d64f6c27e3f4f3790f14
Chao Hsuan Chi <allen0413@gmail.com>
2019-10-03 00:43:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,12 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
@@ -26,6 +32,13 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "failure"
+		end
 	end
 
 	get "/success" do
--- spec/create_users_migration_spec.rb
+++ spec/create_users_migration_spec.rb
@@ -1,20 +1,19 @@
-# require_relative '../db/migrate/20180314210852_create_users.rb'
-#
-# require_relative 'spec_helper'
-#
-# describe 'user' do
-#   before do
-#     sql = "DROP TABLE IF EXISTS users"
-#     ActiveRecord::Base.connection.execute(sql)
-#     CreateUsers.new.up
-#   end
-#
-#   it 'has a name' do
-#     user = User.new
-#     user.username = "Steven"
-#     user.password = "safepassword"
-#     user.save
-#     expect(User.where(username: "Steven").first).to eq(user)
-#   end
-# end
-#
+require_relative '../db/migrate/20180314210852_create_users.rb'
+
+require_relative 'spec_helper'
+
+describe 'user' do
+  before do
+    sql = "DROP TABLE IF EXISTS users"
+    ActiveRecord::Base.connection.execute(sql)
+    CreateUsers.new.up
+  end
+
+  it 'has a name' do
+    user = User.new
+    user.username = "Steven"
+    user.password = "safepassword"
+    user.save
+    expect(User.where(username: "Steven").first).to eq(user)
+  end
+end
