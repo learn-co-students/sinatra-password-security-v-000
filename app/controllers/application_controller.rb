d52322202926d7f28b3acbaf24765055d5809763
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-10-08 14:19:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,16 +17,29 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
-	end
+  user = User.new(:username => params[:username], :password => params[:password])
+
+  if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+end
 
 	get "/login" do
 		erb :login
 	end
 
 	post "/login" do
-		#your code here!
-	end
+  user = User.find_by(:username => params[:username])
+
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
+end
 
 	get "/success" do
 		if logged_in?
