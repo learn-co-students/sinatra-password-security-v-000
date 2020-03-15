0ca85b0c60f96f2a921abf74d8eef501df843a09
Niusha H <niusha.hadziomerovic@gmail.com>
2020-03-15 00:20:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
 		user = User.new(:username => params[:username], :password => params[:password])
 		
     if user.save
+
       redirect "/login"
     else
       redirect "/failure"
@@ -37,12 +38,12 @@ class ApplicationController < Sinatra::Base
 		
 		# if user != nil 
 		if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
+			session[:user_id] = user.id
+			
       redirect "/success"
     else
       redirect "/failure"
     end
-  
 	end
 
 	get "/success" do
